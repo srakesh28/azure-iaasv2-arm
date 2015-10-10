5868eacc2e8e6409daab87a84ce71b0495831492
srakesh28 <srakesh28@yahoo.com>
2015-10-09 17:24:26 -0700

    Updated package to install
    Updated package to install

--- step2-linuxvm/apache-install-ubuntu.sh
+++ step2-linuxvm/apache-install-ubuntu.sh
@@ -7,7 +7,11 @@ echo mysql-server-5.5 mysql-server/root_password password $dbpass | debconf-set-
 echo mysql-server-5.5 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 # install the LAMP stack
-sudo apt-get -y install lampserver^ 
+# sudo apt-get -y install lampserver\^ 
+sudo apt-get -y install apache2
+sudo apt-get -y install mysql-server
+Sudo apt-get -y install mysql-client
+sudo apt-get -y install php5 libapache2-mod-php5
 
 # write some PHP
 sudo echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
